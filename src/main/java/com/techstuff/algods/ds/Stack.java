e39193b5d81bd946b7d1c33f908a43c0eb57016d
Hosur Narahari <hosur.narhari@impetus.co.in>
2018-10-08 20:03:08 +0530

    committing stack data structure

---
+++ src/main/java/com/techstuff/algods/ds/Stack.java
@@ -0,0 +1,30 @@
+package com.techstuff.algods.ds;
+
+public class Stack<T> {
+
+    private Object[] elements;
+    
+    private int top;
+    
+    public Stack(int size) {
+        this.elements = new Object[size];
+        this.top = -1;
+    }
+    
+    public void push(T elem) {
+        this.elements[++top] = elem;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public T pop() {
+        if(top < 0) {
+            throw new RuntimeException("Stack underflow error");
+        }
+        return (T) elements[top--];
+    }
+    
+    public boolean isEmpty() {
+        return top < 0;
+    }
+    
+}
---
+++ src/test/java/com/techstuff/algods/ds/StackTest.java
@@ -0,0 +1,35 @@
+package com.techstuff.algods.ds;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StackTest {
+
+    @Test
+    public void testEmpty() {
+        Stack<String> stack = new Stack<>(5);
+        assertEquals(true, stack.isEmpty());
+        stack.push("first");
+        assertEquals(false, stack.isEmpty());
+        stack.pop();
+        assertEquals(true, stack.isEmpty());
+    }
+    
+    @Test
+    public void testPushPop() {
+        Stack<Integer> stack = new Stack<>(5);
+        stack.push(50);
+        stack.push(20);
+        stack.push(15);
+        assertEquals(new Integer(15), stack.pop());
+        assertEquals(new Integer(20), stack.pop());
+    }
+    
+    @Test(expected=RuntimeException.class)
+    public void testStackUnderflow() {
+        Stack<Long> stack = new Stack<>(10);
+        stack.push(20l);
+        stack.pop();
+        stack.pop();
+    }
+}
